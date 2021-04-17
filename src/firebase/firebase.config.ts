15f01a0444926fe43c1f89e5b112a103e8cfd41c
github akito-10 <akt.fukuda@gmail.com>
2021-04-17 21:04:03 +0900

    firebaseのセットアップ

---
+++ src/firebase/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXqb26asia2wxMzxd6GI4sRSrp4axCjM8",
+  authDomain: "chat-app-ac389.firebaseapp.com",
+  projectId: "chat-app-ac389",
+  storageBucket: "chat-app-ac389.appspot.com",
+  messagingSenderId: "198361379759",
+  appId: "1:198361379759:web:98d126855fd0728000b55a"
+};
